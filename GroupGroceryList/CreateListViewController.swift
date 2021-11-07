4a71ffa1d780c2517a00633d7819c4c5962f395c
Gregbgarman <gregory.b.garman@gmail.com>
2021-11-06 21:02:03 -0400

    adding users to list

---
+++ GroupGroceryList/CreateListViewController.swift
@@ -0,0 +1,54 @@
+//
+//  CreateListViewController.swift
+//  GroupGroceryList
+//
+//  Created by Greg Garman on 11/6/21.
+//
+
+import UIKit
+
+class CreateListViewController: UIViewController {
+
+    
+    @IBOutlet weak var btnSubmitListName: UIButton!
+    
+    
+    @IBAction func btnSubmitListName(_ sender: Any) {
+    
+        self.performSegue(withIdentifier: "AddUsersToListSegue", sender: nil)
+    }
+    
+    @IBOutlet weak var TFListName: UITextField!
+    
+ 
+    @IBAction func TFListNameListener(_ sender: Any) {
+        print("running")
+        let textcount = TFListName.text?.count ?? 0
+        
+        if textcount > 0 {
+            btnSubmitListName.isHidden = false
+        }
+        else{
+            btnSubmitListName.isHidden = true
+        }
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        btnSubmitListName.isHidden = true
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.destination is AddUserController{
+            let VC = segue.destination as? AddUserController
+            VC?.listname = TFListName.text!
+        }
+    }
+   
+
+}
--- GroupGroceryList/EachUserCell.swift
+++ GroupGroceryList/EachUserCell.swift
@@ -6,18 +6,51 @@
 //
 
 import UIKit
+import Parse
+
+protocol UserCellDelegate: AnyObject {
+    func TappedCellbtn(with user: PFObject)
+}
 
 class EachUserCell: UITableViewCell {
 
+    weak var delegate: UserCellDelegate?
+    private var user: PFObject!
+    
     @IBOutlet weak var ivProfilePic: UIImageView!
     
     @IBOutlet weak var TLUserName: UILabel!
     
+    @IBOutlet weak var btnSelectUser: UIButton!
+    
+    
+    
     @IBAction func btnSelectUser(_ sender: Any) {
-        print("clicking")
+        
+        print("here")
+        
+        if btnSelectUser.currentTitle == "Add to List"{
+            btnSelectUser.setTitle("Remove", for: .normal)
+            btnSelectUser.setTitleColor(UIColor.red, for: .normal)
+        }
+        else{
+            btnSelectUser.setTitle("Add to List", for: .normal)
+            btnSelectUser.setTitleColor(UIColor.systemBlue, for: .normal)
+        }
+        
+        
+        
+    
+        delegate?.TappedCellbtn(with: user)
     }
     
-    override func awakeFromNib() {
+    func configure(with user: PFObject){
+        self.user = user
+      
+    }
+    
+    
+    override func awakeFromNib() {      //views not loaded yet when this runs
         super.awakeFromNib()
         // Initialization code
     }
