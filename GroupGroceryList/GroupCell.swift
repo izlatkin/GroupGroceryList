b462a4c07ef12f97155f184d75c670abcafaaf84
Kitty0567 <kem21@my.fsu.edu>
2021-11-08 09:45:57 -0500

    Added MyProfile Screen

---
+++ GroupGroceryList/GroupCell.swift
@@ -0,0 +1,23 @@
+//
+//  GroupCell.swift
+//  GroupGroceryList
+//
+//  Created by Katy Merritt on 11/8/21.
+//
+
+import UIKit
+
+class GroupCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
