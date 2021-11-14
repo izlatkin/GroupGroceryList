29bc34687286145c71a4168cf1ae3ac7fa0dda41
izlatkin <zlatkin_ilya@mail.ru>
2021-11-14 10:21:50 -0500

    performance improvment for Search Item Screen and bug fix

--- GroupGroceryList/ItemSearchController.swift
+++ GroupGroceryList/ItemSearchController.swift
@@ -7,105 +7,101 @@
 
 import UIKit
 
-class ItemSearchController: UIViewController, UITableViewDataSource,UITableViewDelegate {
-    var raw_items = [[String:Any]]()
-    @IBOutlet weak var ItemSearchText: UITextField!
+class ItemSearchController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var rawItems = [[String:Any]]()
     
+//    @IBOutlet weak var itemSearchTextField: UITextField!
+//    @IBOutlet weak var itemsTableView: UITableView!
     
-    @IBOutlet weak var AddItem: UITableView!
+    @IBOutlet weak var itemSearchTextField: UITextField!
+    
+    @IBOutlet weak var itemsTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        AddItem.dataSource = self
-        AddItem.delegate = self
-        self.AddItem.reloadData()
-
-        // Do any additional setup after loading the view.
+        
+        itemsTableView.dataSource = self
+        itemsTableView.delegate = self
+        self.itemsTableView.reloadData()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return raw_items.count
+        return rawItems.count
         //return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell") as! ItemCell
-        var tmp_str = raw_items[indexPath.row]["title"] as! String
+        var tmp_str = rawItems[indexPath.row]["title"] as! String
         var str = tmp_str.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression, range: nil)
         cell.ItemName.text = str
-        tmp_str = raw_items[indexPath.row]["description"] as! String
+        tmp_str = rawItems[indexPath.row]["description"] as! String
         str = tmp_str.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression, range: nil)
         cell.DescriptionLabble.text = str
-        let imageURL_string = raw_items[indexPath.row]["imageUrl"] as! String
+        let imageURL_string = rawItems[indexPath.row]["imageUrl"] as! String
         var imageURL = URL(string: imageURL_string )
         cell.ImageOfItem.af_setImage(withURL: imageURL!)
         return cell
     }
     
     @IBAction func SearchItem(_ sender: Any) {
-        print("searching for item \(ItemSearchText.text)")
-        let str = ItemSearchText.text
-        if str?.count ?? 0 >= 2{
-
-            search(s: str!.trimmingCharacters(in: .whitespacesAndNewlines))
-            self.AddItem.reloadData()
-            
+        print("searching for item \(String(describing: itemSearchTextField.text))")
+        
+        guard let str = itemSearchTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) else {
+            return
         }
-        else{
-            //self.ItemsList.removeAll()
-            self.raw_items.removeAll()
-            self.AddItem.reloadData()
+        if str.count >= 2 {
+            search(s: str)
         }
+        
+        self.rawItems.removeAll()
+        self.itemsTableView.reloadData()
     }
     
     func search(s: String){
-            guard let url = URL(string: "https://walmart2.p.rapidapi.com/search?query=\(s)") else {
-                return
-            }
-            
-            let headers = [
-                "x-rapidapi-key": "478c5bbd95mshe675fbe01298f00p1f0beajsndf857b06d8f6"
-            ]
-            
-            let request = NSMutableURLRequest(
-                url: url,
-                cachePolicy: .useProtocolCachePolicy,
-                timeoutInterval: 10.0
-            )
-            request.httpMethod = "GET"
-            request.allHTTPHeaderFields = headers
+        guard let url = URL(string: "https://walmart2.p.rapidapi.com/search?query=\(s)") else {
+            return
+        }
+        
+        let headers = [
+            "x-rapidapi-key": "478c5bbd95mshe675fbe01298f00p1f0beajsndf857b06d8f6"
+        ]
+        
+        var request = URLRequest(
+            url: url,
+            cachePolicy: .useProtocolCachePolicy,
+            timeoutInterval: 10.0
+        )
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
 
-            let session = URLSession.shared
-            let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
-                if (error != nil) {
-                    print(error)
-                } else {
-                    if let data = data,
-                       let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request, completionHandler: { (data, response, error) -> Void in
+            if let data = data {
+                do {
+                    let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
+                    if let dataDictionary = jsonObject as? [String: Any],
                        let resultItems = dataDictionary["items"] as? [[String : Any]]
                     {
-                        self.raw_items = resultItems
+                        DispatchQueue.main.async {
+                            self.rawItems = resultItems
+                            self.itemsTableView.reloadData()
+                        }
+                    } else {
+                        print("Corrupted data")
                     }
+                } catch (let error) {
+                    print(error)
                 }
-            })
+            } else {
+                if let error = error {
+                    print(error)
+                } else {
+                    // Imposible case
+                }
+            }
+        })
 
-            dataTask.resume()
-        }
-    
-    
-
-    
-
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        dataTask.resume()
     }
-    */
-
 }
