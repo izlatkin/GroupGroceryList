02b4a2624cb5e53ef6fb80c95a72126ef4efdd8e
Kitty0567 <kem21@my.fsu.edu>
2021-11-22 17:02:14 -0500

    Can Change Color of Settings Screen

--- GroupGroceryList/SettingsViewController.swift
+++ GroupGroceryList/SettingsViewController.swift
@@ -9,23 +9,32 @@ import UIKit
 import Parse
 
 class SettingsViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    let modes = [0,1]
    
     @IBOutlet weak var signOut: UIButton!
+    
+    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var displayName: UILabel!
+    
     @IBOutlet weak var profileImage: UIImageView!
-    @IBOutlet weak var changeProfile: UIButton!
+   
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var changeUsername: UIButton!
     @IBOutlet weak var passwordLabel: UILabel!
     @IBOutlet weak var changePassword: UIButton!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var changeName: UIButton!
-    @IBOutlet weak var modeSlider: UISwitch!
-    @IBOutlet weak var modeIndicator: UILabel!
+   
+
+    @IBOutlet weak var changePicture: UIButton!
+    
+    @IBOutlet weak var viewMode: UISegmentedControl!
     @IBOutlet weak var basicUserLabel: UILabel!
     @IBOutlet weak var basicPasswordLabel: UILabel!
     @IBOutlet weak var basicNameLabel: UILabel!
     
+    @IBOutlet weak var clearLabel: UILabel!
     var om_var = false
     
     
@@ -44,62 +53,89 @@ class SettingsViewController: UIViewController, UINavigationControllerDelegate, 
         
         present(picker, animated: true, completion: nil)
     }
-    
-    @IBAction func modeActivated(_ sender: UISwitch) {
-        
-        if modeSlider.isOn {
-            modeIndicator.text = "Dark Mode Activated"
-            self.om_var = true
-            
-        }
-        else {
-            modeIndicator.text = "Dark Mode Off"
-            self.om_var = false
-        }
-    }
+       
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let defaults = UserDefaults.standard
-        
-        let on = defaults.bool(forKey: "myBool")
-        if on == true{
-            modeSlider.setOn(true, animated: true)
-            self.view.backgroundColor = UIColor.black
-            displayName.textColor = UIColor.white
-            usernameLabel.textColor = UIColor.white
-            passwordLabel.textColor = UIColor.white
-            nameLabel.textColor = UIColor.white
-            modeIndicator.textColor = UIColor.white
-            basicUserLabel.textColor = UIColor.white
-            basicPasswordLabel.textColor = UIColor.white
-            basicNameLabel.textColor = UIColor.white
-        }else{
-            modeSlider.setOn(false, animated: true)
-            self.view.backgroundColor = UIColor.white
-            displayName.textColor = UIColor.black
-            usernameLabel.textColor = UIColor.black
-            passwordLabel.textColor = UIColor.black
-            nameLabel.textColor = UIColor.white
-            modeIndicator.textColor = UIColor.black
-            basicUserLabel.textColor = UIColor.black
-            basicPasswordLabel.textColor = UIColor.black
-            basicNameLabel.textColor = UIColor.black
-        }
-        
-    
-        
+        let modeValue = defaults.double(forKey: "myInt")
+               viewMode.selectedSegmentIndex = Int(modeValue)
+               if (modeValue == 0)
+               {
+                   self.view.backgroundColor = UIColor.white
+                   titleLabel.textColor = UIColor.black
+                   displayName.textColor = UIColor.black
+                   usernameLabel.textColor = UIColor.black
+                   passwordLabel.textColor = UIColor.black
+                   nameLabel.textColor = UIColor.black
+                   basicUserLabel.textColor = UIColor.black
+                   basicPasswordLabel.textColor = UIColor.black
+                   basicNameLabel.textColor = UIColor.black
+                   
+               }
+               if (modeValue == 1)
+               {
+                   self.view.backgroundColor = UIColor.black
+                   titleLabel.textColor = UIColor.white
+                   displayName.textColor = UIColor.white
+                   usernameLabel.textColor = UIColor.white
+                   passwordLabel.textColor = UIColor.white
+                   nameLabel.textColor = UIColor.white
+                   basicUserLabel.textColor = UIColor.white
+                   basicPasswordLabel.textColor = UIColor.white
+                   basicNameLabel.textColor = UIColor.white
+                   
+               }
     }
+
+        let defaults = UserDefaults.standard
+
+      
     
-    @IBAction func LogOut(_ sender: Any) {
+        
+    
+    @IBAction func ColorChanger(_ sender: UISegmentedControl) {
+        let modevalue = modes[viewMode.selectedSegmentIndex]
+                clearLabel.text = "\(modevalue)"
+                defaults.set(clearLabel.text, forKey: "myInt")
+                if (modevalue == 0)
+                {
+                    self.view.backgroundColor = UIColor.white
+                    titleLabel.textColor = UIColor.black
+                    displayName.textColor = UIColor.black
+                    usernameLabel.textColor = UIColor.black
+                    passwordLabel.textColor = UIColor.black
+                    nameLabel.textColor = UIColor.black
+                    basicUserLabel.textColor = UIColor.black
+                    basicPasswordLabel.textColor = UIColor.black
+                    basicNameLabel.textColor = UIColor.black
+                    
+                }
+                if (modevalue == 1)
+                {
+                    self.view.backgroundColor = UIColor.black
+                    titleLabel.textColor = UIColor.white
+                    displayName.textColor = UIColor.white
+                    usernameLabel.textColor = UIColor.white
+                    passwordLabel.textColor = UIColor.white
+                    nameLabel.textColor = UIColor.white
+                    basicUserLabel.textColor = UIColor.white
+                    basicPasswordLabel.textColor = UIColor.white
+                    basicNameLabel.textColor = UIColor.white
+                    
+                }
+                defaults.synchronize()
+
+    }
+    /*
+    
+    @IBAction func LogOut(_ sender: UIButton) {
         PFUser.logOut()
         let main = UIStoryboard(name: "Main", bundle: nil)
         let loginViewController = main.instantiateViewController(identifier: "LoginViewController")
         guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else { return }
         delegate.window?.rootViewController = loginViewController
-        
     }
+     */
     /*
     // MARK: - Navigation
 
---
+++ Pods/Target Support Files/Pods-GroupGroceryList/Pods-GroupGroceryList-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GroupGroceryList : NSObject
+@end
+@implementation PodsDummy_Pods_GroupGroceryList
+@end
